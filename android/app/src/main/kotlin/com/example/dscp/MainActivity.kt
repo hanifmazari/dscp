0156df96855a0cc60d972f1afa93df3f4e5c275b
hanifmazari <hanifmazari2010@gmail.com>
2021-09-21 20:47:42 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/dscp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dscp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Home()),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hanif"),
+      ),
+      body: WebView(
+        initialUrl: 'http://116.71.135.202/LeavePortal/',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
